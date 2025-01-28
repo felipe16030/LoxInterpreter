96f74ae357ac67d5d08c951aa0740f7bc2d45b04
felipe16030 <felipebergerman@gmail.com>
2025-01-27 21:42:22 -0500

    Continues work on the parser

--- Chapter6.md
+++ Chapter6.md
@@ -12,9 +12,9 @@ It produces the same string but not the same syntax tree which means different e
 The way we deal with this is:
 1. **Precedence** tells us which operator to evaluate first in an expression
 2. **Associativity** tells us which operator is evaluated first in a series of the *same* operator
-   1. left associative: $a + b + c \equiv (a + b) + c$
-   2. right associative: $a = b = c \equiv a = (b = c)$
-      1. only unary operators are right associative
+	1. left associative: $a + b + c \equiv (a + b) + c$
+	2. right associative: $a = b = c \equiv a = (b = c)$
+		1. only unary operators are right associative
 
 Without these, an expression that uses multiple operators is ambiguous
 
@@ -42,3 +42,29 @@ and support strong error handling.
 It starts from the top outtermost grammar rule (expression) and matches downward.
 
 Therefore, the parser is a literal translation of a grammar's rules straight into imperative code.
+
+### Syntax Errors
+
+A parser has 2 jobs: given a valid series of tokens, produce syntax tree AND given an invalid series of tokens, produce an error and tell the user of their mistake.
+
+When the parser runs into a syntax error, it must:
+1. Detect and report the error before making a syntax tree
+2. Cannot crash or hang when detecting an error
+
+A decent parser must also be:
+1. Fast such that it allows for live syntax highlighting and reparsing in milliseconds
+2. Report as many distincy errors as there are
+3. Minimize cascaded errors. get back on track and see if their are other independent errors throughout the source code. The way the parser does this is called **error recovery**
+
+#### Panic Mode Error Recovery
+
+Panic Mode is the error recovery method that has best withstood the test of time.
+
+Once an incoherent token is discovered, the parser enters panic mode. It **synchronizes** its state with the incoming tokens: it does this by jumping out of any nested productions and discards tokens until reaching one that can appear at that point in the rule. This means that we may miss some errors in the discarded tokens but do not report cascaded errors.
+
+#### Synchronizing a recursive descent parser
+
+We can use Java's call stack to check on what the parser is doing. Each rule is a frame on the stack and to synchronize, clear the frames.
+
+To synchronize, throw the ParseError exception which is caught higher up in the method for the grammar rule.
+
--- lox/Parser.java
+++ lox/Parser.java
@@ -4,6 +4,8 @@ import java.util.List;
 import static lox.TokenType.*;
 
 public abstract class Parser {
+    private static class ParseError extends RuntimeException {}
+
     private final List<Token> tokens;
     private int current = 0;
 
@@ -23,6 +25,7 @@ public abstract class Parser {
         // the "*" part of the rule that matches every comparison until the end of the expression
         // for example: a == b == c == d creates a new binary expression each iteration with the 
         // old expression as the left expression
+        // if the parser does not hit an equality operator, then it effectively calls and returns comparison()
         while(match(BANG_EQUAL, EQUAL)) {
             Token operator = previous();
             Expr right = comparison();
@@ -30,6 +33,64 @@ public abstract class Parser {
         }
 
         return expr;
+    }
+
+    private Expr comparison() {
+        Expr expr = term();
+
+        while(match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+
+        while(match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr factor() {
+        Expr expr = unary();
+
+        while(match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        if(match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+        if (match(NUMBER, STRING)) return new Expr.Literal(previous().literal);
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
     }
 
     // This checks to see if the current token has any of the given types
@@ -42,6 +103,12 @@ public abstract class Parser {
         }
 
         return false;
+    }
+
+    private Token consume(TokenType token, String message) {
+        if (check(token)) return advance();
+
+        throw error(peek(), message);
     }
 
     private boolean check(TokenType type) {
@@ -64,5 +131,33 @@ public abstract class Parser {
 
     private Token previous() {
         return this.tokens.get(this.current - 1);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    // To be called after catching a ParseError
+    private void synchronize() {
+        advance();
+
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) return;
+
+            switch(peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+            }
+
+            advance();
+        }
     }
 }
